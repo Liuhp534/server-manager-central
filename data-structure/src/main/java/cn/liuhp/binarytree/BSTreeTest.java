7f9ac2ed1c07a643d4b21b137d669eb9e4d12752
刘辉鹏 <liuhuipeng@huize.com>
2019-05-27 16:42:24 +0800

    PECS，就是生成者extends，消费者super；

--- data-structure/src/main/java/cn/liuhp/binarytree/BSTreeTest.java
+++ data-structure/src/main/java/cn/liuhp/binarytree/BSTreeTest.java
@@ -1,7 +1,9 @@
 package cn.liuhp.binarytree;
 
+import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.List;
+import java.util.TreeMap;
 
 /**
  * @description: 测试类
@@ -12,7 +14,7 @@ public class BSTreeTest {
 
 
     public static void main(String[] args) {
-        //m1();
+        m1();
         m2();
     }
 
@@ -22,12 +24,13 @@ public class BSTreeTest {
         for (Integer i : list) {
             tree.insert(i);
         }
-        System.out.println(tree.search(9));
+        System.out.println(tree.search(9));//返回的是private,所以外面不能访问内部类的方法.
     }
 
     private static void m1() {
         BSTree<Integer> tree = new BSTree<>();
-        List<Integer> list = Arrays.asList(7, 4, 1, 9, 0, 2, 6, 3, 5, 8);
+        //List<Integer> list = Arrays.asList(7, 4, 1, 9, 0, 2, 6, 3, 5, 8);
+        List<Integer> list = Arrays.asList(1, 5, 4, 3, 2, 6);
         for (Integer i : list) {
             tree.insert(i);
         }
--- data-structure/src/main/java/cn/liuhp/stack/EStack.java
+++ data-structure/src/main/java/cn/liuhp/stack/EStack.java
@@ -1,6 +1,6 @@
 package cn.liuhp.stack;
 
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * @description: 泛型栈
@@ -11,6 +11,52 @@ public class EStack<E> {
 
     /*测试栈*/
     public static void main(String[] args) {
+        test2();
+    }
+
+    /*泛型可以支持子类*/
+    private static void test4() {
+        EStack<Number> eStack = new EStack<>();
+        eStack.push(new Integer(1));
+        Collection<Object> c = new ArrayList<>();
+        eStack.popAll(c);//consumer 需要使用super
+    }
+
+    /*泛型可以支持子类*/
+    private static void test3() {
+        EStack<Number> eStack = new EStack<>();
+        eStack.push(new Integer(1));
+        Iterable<Integer> integers = new ArrayList<>();
+        /*
+        * public void pushAll(Iterable<E> it) {
+        for (E e : it) {
+            this.push(e);
+        }
+    }
+        * */
+        //eStack.pushAll(integers);这样不行的
+        eStack.pushAll(integers);//这样就ok了
+        while (!eStack.isEmpty()) {
+            System.out.println(eStack.pop());
+        }
+    }
+
+    /*泛型可以支持子类*/
+    private static void test2() {
+        EStack<List> eStack = new EStack<>();
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        list1.add(1);
+        eStack.push(list1);
+        LinkedList<Integer> list2 = new LinkedList<>();
+        list2.add(2);
+        eStack.push(list2);
+        while (!eStack.isEmpty()) {
+            System.out.println(eStack.pop());
+        }
+    }
+
+    /*测试出栈和入栈*/
+    private static void test1() {
         EStack<String> eStack = new EStack<>();
         for (int i = 0; i < 50; i++) {
             eStack.push(i + "");
@@ -42,6 +88,18 @@ public class EStack<E> {
         ensureCapacity();
         elements[size++] = e;
         return true;
+    }
+
+    public void pushAll(Iterable<? extends E> it) {
+        for (E e : it) {
+            this.push(e);
+        }
+    }
+
+    public void popAll(Collection<? super E> c) {
+        for (int i = 0; i < elements.length; i ++) {
+            c.add(this.pop());
+        }
     }
 
     public void ensureCapacity() {
