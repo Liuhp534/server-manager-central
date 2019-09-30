99710b68a964ccc699038de463a2aa12c2651d8f
刘辉鹏 <liuhuipeng@huize.com>
2019-09-30 14:24:00 +0800

    jackson的简单使用（日期格式转换、是否有null值的属性）

---
+++ springboot-demo/src/main/java/cn/liu/hui/peng/test/TestJsonUtils.java
@@ -0,0 +1,41 @@
+package cn.liu.hui.peng.test;
+
+import cn.liuhp.domain.Person;
+import cn.liuhp.utils.JackJsonUtils;
+
+import java.util.Date;
+import java.util.Map;
+
+public class TestJsonUtils {
+
+    public static void main(String[] args) {
+        m1();
+        m2();
+        m3();
+    }
+
+    private static void m1() {
+        Person person = new Person();
+        person.setName("jeff");
+        person.setAge(100);
+        //person.setBirthday(new Date());
+        String jsonStr = JackJsonUtils.beanToJson(person);
+        System.out.println(jsonStr);
+
+    }
+
+    private static void m2() {
+        Map<String, Object> map = JackJsonUtils.jsonToMap("{\"name\":\"jeff\",\"age\":100,\"birthday\":\"2019-09-30 14:09:22\"}");
+        for (Map.Entry<String, Object> mapt : map.entrySet()) {
+            System.out.println(mapt.getKey() + "_" + mapt.getValue());
+        }
+    }
+
+    private static void m3() {
+        Person person = JackJsonUtils.jsonToBean("{\"name\":\"jeff\",\"age\":100,\"birthday\":\"2019-09-30 14:09:22\"}", Person.class);
+        System.out.println(person);
+    }
+
+
+
+}
---
+++ springboot-demo/src/main/java/cn/liuhp/domain/Person.java
@@ -0,0 +1,16 @@
+package cn.liuhp.domain;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Person {
+
+    private String name;
+
+    private Integer age;
+
+    private Date birthday;
+
+}
