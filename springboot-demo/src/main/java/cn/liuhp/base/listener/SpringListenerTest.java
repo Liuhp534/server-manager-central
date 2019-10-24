979006914878d1bedd2bdc732dc5ee59aabe1218
刘辉鹏 <liuhuipeng@huize.com>
2019-10-24 10:30:36 +0800

    没有泛型的，看看能够执行几次 or 只是ApplicationEvent都会执行多次。打印事件情况

---
+++ springboot-demo/src/main/java/cn/liuhp/base/listener/SpringListenerTest.java
@@ -0,0 +1,21 @@
+package cn.liuhp.base.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+/*
+ * 这个是没有泛型的，看看能够执行几次 or 只是ApplicationEvent都会执行多次（3次）
+ * */
+@Component
+public class SpringListenerTest implements ApplicationListener<ApplicationEvent> {
+
+    private static final Logger logger = LoggerFactory.getLogger(SpringListenerTest.class);
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+        logger.info("无泛型的springlistener。。。。。。{}", event.getClass());
+    }
+}
