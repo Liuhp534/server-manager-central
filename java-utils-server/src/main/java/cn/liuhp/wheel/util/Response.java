c97cd27b22fae44838d731c37c29c4be70ca4a05
刘辉鹏 <liuhuipeng@huize.com>
2019-10-28 09:51:13 +0800

    格式调整

--- java-utils-server/src/main/java/cn/liuhp/wheel/util/Response.java
+++ java-utils-server/src/main/java/cn/liuhp/wheel/util/Response.java
@@ -3,13 +3,13 @@ package cn.liuhp.wheel.util;
 
 /**
  * <p>
- * 
+ * <p>
  * 结果泛型实体
  *
  * </p>
- * @author	hz16092620 
- * @date	2018年9月14日 下午4:59:40
- * @version      
+ *
+ * @author hz16092620
+ * @date 2018年9月14日 下午4:59:40
  */
 public class Response<T> {
 
@@ -18,62 +18,62 @@ public class Response<T> {
     private String errorMsg;
 
     private String status = "00000";
-    
+
     private boolean success = Boolean.TRUE;
-    
+
     /**
      * 构建器
-     * */
+     */
     public static class ResponseBuilder<T> {
 
-	private T result;
+        private T result;
 
-	private String errorMsg;
+        private String errorMsg;
 
-	private String status = "00000";
+        private String status = "00000";
 
-	private boolean success = Boolean.TRUE;
-	
-	public ResponseBuilder<T> setResult(T result) {
-	    this.result = result;
-	    return this;
-	}
-	
-	public ResponseBuilder<T> setErrorMsg(String errorMsg) {
-	    this.errorMsg = errorMsg;
-	    return this;
-	}
-	
-	public ResponseBuilder<T> setStatus(String status) {
-	    this.status = status;
-	    return this;
-	}
-	
-	public ResponseBuilder<T> setSuccess(boolean success) {
-	    this.success = success;
-	    return this;
-	}
+        private boolean success = Boolean.TRUE;
 
-	public Response<T> build() {
-	    return new Response<T>(result, errorMsg, status, success);
-	}
-    }
-    
-    private Response () {
-	
+        public ResponseBuilder<T> setResult(T result) {
+            this.result = result;
+            return this;
+        }
+
+        public ResponseBuilder<T> setErrorMsg(String errorMsg) {
+            this.errorMsg = errorMsg;
+            return this;
+        }
+
+        public ResponseBuilder<T> setStatus(String status) {
+            this.status = status;
+            return this;
+        }
+
+        public ResponseBuilder<T> setSuccess(boolean success) {
+            this.success = success;
+            return this;
+        }
+
+        public Response<T> build() {
+            return new Response<T>(result, errorMsg, status, success);
+        }
     }
 
-    private Response (T result, String errorMsg, String status, boolean success) {
-	this.result = result;
-	this.errorMsg = errorMsg;
-	this.status = status;
-	this.success = success;
+    private Response() {
+
+    }
+
+    private Response(T result, String errorMsg, String status, boolean success) {
+        this.result = result;
+        this.errorMsg = errorMsg;
+        this.status = status;
+        this.success = success;
     }
 
     public T getResult() {
         return result;
     }
-    
+
     public String getErrorMsg() {
         return errorMsg;
     }
@@ -81,7 +81,7 @@ public class Response<T> {
     public String getStatus() {
         return status;
     }
-    
+
     public boolean isSuccess() {
         return success;
     }
@@ -89,12 +89,12 @@ public class Response<T> {
     public static <T> Response<T> success() {
         return new Response<T>();
     }
-    
+
     public static <T> Response<T> fail(String status, String errorMsg) {
-	Response<T> fail = new Response<T>();
-	fail.errorMsg = errorMsg;
-	fail.status = status;
-	fail.success = Boolean.FALSE;
+        Response<T> fail = new Response<T>();
+        fail.errorMsg = errorMsg;
+        fail.status = status;
+        fail.success = Boolean.FALSE;
         return fail;
     }
 }
--- springboot-demo/src/main/java/cn/liuhp/base/listener/SpringListenerTest.java
+++ springboot-demo/src/main/java/cn/liuhp/base/listener/SpringListenerTest.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
  * 这个是没有泛型的，看看能够执行几次 or 只是ApplicationEvent都会执行多次（3次）
  * */
 @Component
-public class SpringListenerTest implements ApplicationListener<ApplicationEvent> {
+public class SpringListenerTest implements ApplicationListener {
 
     private static final Logger logger = LoggerFactory.getLogger(SpringListenerTest.class);
 
