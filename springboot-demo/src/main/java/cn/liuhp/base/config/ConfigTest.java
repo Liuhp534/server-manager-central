dde19efe7c1802f7d4b1c86c3d7be18fb1b8f4bd
刘辉鹏 <liuhuipeng@huize.com>
2019-12-02 19:20:25 +0800

    boot，javaconfig的使用

--- springboot-demo/src/main/java/cn/liuhp/base/config/ConfigTest.java
+++ springboot-demo/src/main/java/cn/liuhp/base/config/ConfigTest.java
@@ -8,17 +8,18 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-
+/*
+* 是作为一个组件，默认值，直接填写
+* */
 @Component
-
 @ConfigurationProperties(prefix = "testConfig")
 @PropertySource("classpath:configTest.properties")
 @Data
 public class ConfigTest {
 
-    private String name;
+    private String name = "john";
 
-    private int age;
+    private int age = 100;
 
     private List<String> likes;
 
