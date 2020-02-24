20f3a82162ec81cef4e7137d8f2fa4982eef579a
Liuhp534 <617456341@qq.com>
2020-02-24 10:12:00 +0800

    junit类

---
+++ springboot-demo/src/test/java/cn/liuhp/PropertiesConfigTest.java
@@ -0,0 +1,49 @@
+package cn.liuhp;
+
+import cn.liuhp.base.config.ConfigurationPropertiesTest;
+import cn.liuhp.domain.XmlCreateBean;
+import cn.liuhp.utils.JackJsonUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+/**
+ * SpringBoot单元测试;
+ *
+ * 可以在测试期间很方便的类似编码一样进行自动注入等容器的功能
+ *
+ */
+
+public class PropertiesConfigTest extends BaseJunit {
+
+	@Autowired
+	ApplicationContext ioc;
+
+	@Autowired
+	ConfigurationPropertiesTest configurationPropertiesTest;
+
+	@Autowired
+	XmlCreateBean xmlCreateBean;
+
+	@Test
+	public void testConfigTest() {
+		System.out.println(JackJsonUtils.beanToJson(configurationPropertiesTest));
+	}
+
+	@Test
+	public void testXmlCreateBean() {
+		System.out.println(JackJsonUtils.beanToJson(xmlCreateBean));
+	}
+
+	@Test
+	public void testIoc(){
+		boolean b = ioc.containsBean("xmlCreateBean");
+		System.out.println(b);
+	}
+
+
+}
